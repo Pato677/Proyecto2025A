2b581fe122f6e1f3376f02909afdf47b9612779a
Mathew <mathew_gutierrez@outlook.com>
2025-07-19 14:03:43 -0500

    feat: Add Sequelize configuration for database connection and synchronization

---
+++ server/config/sequelize.config.js
@@ -0,0 +1,36 @@
+const {Sequelize} = require("sequelize");
+
+const username = "root";
+//const password = "root";
+const password = "admin";
+const bdd_name = "TransportesEC";
+const hostName = "localhost";
+
+//Conexión incial sin especificar la base de datos
+const initialSequelize = new Sequelize(`mysql://${username}:${password}@localhost`);
+
+initialSequelize.query(`CREATE DATABASE IF NOT EXISTS ${bdd_name};`)
+    .then(() => console.log("DBB creada o ya existía"))
+    .catch((error) => {
+        console.error("Error al crear la BDD", error);
+        process.exit(1);
+    });
+
+const sequelize = new Sequelize(bdd_name, username, password, {
+    host: hostName,
+    dialect: "mysql",
+});
+
+//EN el sync si se utiliza el parametro de entrafa force: true, se eliminan las tablas existentes y se crean de nuevo
+//Con el alter:true se actualizan las tablas existentes sin perder los datos, pero puede causar problemas si hay cambios incompatibles (Hace el mejor intento de mantener los datos)
+//Lo recomendable en el desarrollo es usar force true
+//En producción no se debe utilizar ninguno.
+
+//sequelize.sync({force:true}).then(() => {
+sequelize.sync().then(() => {
+    console.log(`Base de datos ${bdd_name} sincronizada`);
+}).catch((error) => {
+    console.error(`Error al sincronizar la base de datos ${bdd_name}`, error);
+});
+
+module.exports = sequelize;
