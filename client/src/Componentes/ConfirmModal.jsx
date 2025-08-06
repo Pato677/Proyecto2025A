9d76675caae67b8b602baa171e02a7bfb041b136
JORDY PATRICIO MAYORGA NUNEZ <jordy.mayorga@epn.edu.ec>
2025-08-06 11:33:42 -0500

    Jordy Mayorga - Edicion de los archivos css para los alert

--- client/src/Componentes/ConfirmModal.jsx
+++ client/src/Componentes/ConfirmModal.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import './Estilos/Modal.css';
-import Button from './Button';
 
 function ConfirmModal({ open, title, children, onCancel, onConfirm }) {
   if (!open) return null;
   return (
     <div className="modal-overlay">
       <div className="modal-content">
-        <h3 style={{ marginBottom: '1.2rem', fontWeight: 600 }}>{title}</h3>
+        <h3>{title}</h3>
         {children}
         <div className="modal-actions">
-          <Button text="Cancelar" width="120px" onClick={onCancel} />
-          <Button text="Eliminar" width="120px" onClick={onConfirm} />
+          <button onClick={onCancel}>
+            Cancelar
+          </button>
+          <button onClick={onConfirm}>
+            Eliminar
+          </button>
         </div>
       </div>
     </div>
